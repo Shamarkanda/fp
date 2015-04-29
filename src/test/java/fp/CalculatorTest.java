ca13f41610569b200dfafe89eaefe6dd15567a37
Jozu Crespo <jozucrespo@gmail.com>
2015-04-29 18:51:16 +0200

    Cambios en los metodos JUnit.Assert para mayor claridad al leer.
    Signed-off-by: Jozu Crespo <jozucrespo@gmail.com>

--- src/test/java/fp/CalculatorTest.java
+++ src/test/java/fp/CalculatorTest.java
@@ -7,63 +7,58 @@ import static fp.Calculator.isValidDate;
 import static fp.Calculator.sin;
 import static fp.Calculator.speakToMe;
 import static fp.Calculator.stepThisNumber;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class CalculatorTest {
 
 	@Test
 	public void testSin() {
-		assertNotNull(sin(0));
-		assertEquals(sin(30), Double.valueOf(0.5));
-		assertEquals(sin(90), Double.valueOf(1));
-		assertEquals(sin(270), Double.valueOf(-1));
-		assertEquals(sin(810), Double.valueOf(1));
+		Assert.assertNotNull(sin(0));
+		Assert.assertEquals(sin(30), Double.valueOf(0.5));
+		Assert.assertEquals(sin(90), Double.valueOf(1));
+		Assert.assertEquals(sin(270), Double.valueOf(-1));
+		Assert.assertEquals(sin(810), Double.valueOf(1));
 		System.out.println("1P");
 	}
 
 	@Test
 	public void testStepThisNumber() {
-		assertNotNull(stepThisNumber(0, 0));
-		assertArrayEquals(stepThisNumber(5, 1), new int[] { 4, 3, 2, 1 });
-		assertArrayEquals(stepThisNumber(5, 2), new int[] { 3, 1 });
-		assertArrayEquals(stepThisNumber(12, 3), new int[] { 9, 6, 3 });
+		Assert.assertNotNull(stepThisNumber(0, 0));
+		Assert.assertArrayEquals(stepThisNumber(5, 1), new int[] { 4, 3, 2, 1 });
+		Assert.assertArrayEquals(stepThisNumber(5, 2), new int[] { 3, 1 });
+		Assert.assertArrayEquals(stepThisNumber(12, 3), new int[] { 9, 6, 3 });
 		System.out.println("1P");
 	}
 
 	@Test
 	public void testDivisors() {
-		assertNull(divisors(0));
-		assertArrayEquals(divisors(1), new int[] { 1 });
-		assertArrayEquals(divisors(12), new int[] { 12, 6, 4, 3, 2, 1 });
-		assertArrayEquals(divisors(20), new int[] { 20, 10, 5, 2, 1 });
+		Assert.assertNull(divisors(0));
+		Assert.assertArrayEquals(divisors(1), new int[] { 1 });
+		Assert.assertArrayEquals(divisors(12), new int[] { 12, 6, 4, 3, 2, 1 });
+		Assert.assertArrayEquals(divisors(20), new int[] { 20, 10, 5, 2, 1 });
 		System.out.println("1P");
 	}
 
 	@Test
 	public void testCheckMyBet() {
-		assertNotNull(checkMyBet(null, null));
-		assertEquals(
+		Assert.assertNotNull(checkMyBet(null, null));
+		Assert.assertEquals(
 				checkMyBet(Arrays.asList(1, 2, 3, 4, 5, 6),
 						Arrays.asList(1, 2, 3, 4, 5, 6)), Integer.valueOf(6));
-		assertEquals(
+		Assert.assertEquals(
 				checkMyBet(Arrays.asList(2, 1, 4, 3, 6, 5),
 						Arrays.asList(1, 2, 3, 4, 5, 6)), Integer.valueOf(0));
-		assertEquals(
+		Assert.assertEquals(
 				checkMyBet(Arrays.asList(1, 2, 3, 4, 5, 6),
 						Arrays.asList(5, 5, 5, 5, 5, 5)), Integer.valueOf(1));
-		assertEquals(
+		Assert.assertEquals(
 				checkMyBet(Arrays.asList(1, 2, 3, 4, 7, 6),
 						Arrays.asList(5, 5, 5, 5, 5, 5)), Integer.valueOf(0));
-		assertEquals(
+		Assert.assertEquals(
 				checkMyBet(Arrays.asList(1, 1, 2, 2, 3, 3),
 						Arrays.asList(6, 6, 2, 5, 3, 5)), Integer.valueOf(2));
 		System.out.println("1P");
@@ -71,41 +66,41 @@ public class CalculatorTest {
 
 	@Test
 	public void testSpeakToMe() {
-		assertNotNull(speakToMe(0));
-		assertEquals(speakToMe(0), "Cero");
-		assertEquals(speakToMe(10), "Diez");
-		assertEquals(speakToMe(20), "Veinte");
-		assertEquals(speakToMe(60), "Sesenta");
-		assertEquals(speakToMe(61), "Sesenta y uno");
-		assertEquals(speakToMe(90), "Noventa");
-		assertEquals(speakToMe(93), "Noventa y tres");
+		Assert.assertNotNull(speakToMe(0));
+		Assert.assertEquals(speakToMe(0), "Cero");
+		Assert.assertEquals(speakToMe(10), "Diez");
+		Assert.assertEquals(speakToMe(20), "Veinte");
+		Assert.assertEquals(speakToMe(60), "Sesenta");
+		Assert.assertEquals(speakToMe(61), "Sesenta y uno");
+		Assert.assertEquals(speakToMe(90), "Noventa");
+		Assert.assertEquals(speakToMe(93), "Noventa y tres");
 		System.out.println("2P");
 	}
 
 	@Test
 	public void testIsLeapYear() {
-		assertNotNull(isLeapYear(""));
-		assertTrue(isLeapYear("01-01-1904"));
-		assertTrue(isLeapYear("01-01-1928"));
-		assertFalse(isLeapYear("01-01-2100"));
-		assertTrue(isLeapYear("01-01-2000"));
+		Assert.assertNotNull(isLeapYear(""));
+		Assert.assertTrue(isLeapYear("01-01-1904"));
+		Assert.assertTrue(isLeapYear("01-01-1928"));
+		Assert.assertFalse(isLeapYear("01-01-2100"));
+		Assert.assertTrue(isLeapYear("01-01-2000"));
 		System.out.println("1P");
 	}
 
 	@Test
 	public void testIsValidDate() {
-		assertNotNull(isValidDate(""));
-		assertTrue(isValidDate("01-01-0001"));
-		assertTrue(isValidDate("31-12-2015"));
-		assertFalse(isValidDate(""));
-		assertFalse(isValidDate("asdasd"));
-		assertFalse(isValidDate("21-12"));
-		assertFalse(isValidDate("12-2000"));
-		assertFalse(isValidDate("00-12-2000"));
-		assertFalse(isValidDate("32-12-2000"));
-		assertFalse(isValidDate("31-13-2000"));
-		assertFalse(isValidDate("01-00-2000"));
-		assertFalse(isValidDate("01-01-0000"));
+		Assert.assertNotNull(isValidDate(""));
+		Assert.assertTrue(isValidDate("01-01-0001"));
+		Assert.assertTrue(isValidDate("31-12-2015"));
+		Assert.assertFalse(isValidDate(""));
+		Assert.assertFalse(isValidDate("asdasd"));
+		Assert.assertFalse(isValidDate("21-12"));
+		Assert.assertFalse(isValidDate("12-2000"));
+		Assert.assertFalse(isValidDate("00-12-2000"));
+		Assert.assertFalse(isValidDate("32-12-2000"));
+		Assert.assertFalse(isValidDate("31-13-2000"));
+		Assert.assertFalse(isValidDate("01-00-2000"));
+		Assert.assertFalse(isValidDate("01-01-0000"));
 		System.out.println("2P");
 	}
 
